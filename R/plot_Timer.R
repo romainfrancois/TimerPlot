3dc5c93953efcb594d11808d734eccd1f80021a0
Romain Francois <romain@r-enthusiasts.com>
2014-07-29 00:20:17 +0200

    add box

--- R/plot_Timer.R
+++ R/plot_Timer.R
@@ -13,7 +13,6 @@ plot.Timer <- function(data, colors =
     ylim <- c(0.5, length(data) + .5)
     plot( 0, type = "n", xlim = xlim, ylim = ylim, 
         axes = FALSE, ann = FALSE, yaxs = "i", xaxs = "i" )
-    
     for( i in 1:length(data) ){
         x <- c( "start" = 0, data[[i]] )
         n <- length(x)
@@ -26,6 +25,7 @@ plot.Timer <- function(data, colors =
           lwd = .5
           )   
     }
+    box()
     
 }
 
